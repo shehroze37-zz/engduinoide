be958bc9fb182751343127ff4ff0796486c42bc2
Shehroze Bhatti <playboyshehroze@yahoo.com>
2013-10-23 15:46:14 +0100

    CSS elements added

--- search.php
+++ search.php
@@ -3,6 +3,21 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Search Form</title>
+
+<style type="text/css">
+    body { background-color: #fff; border-top: solid 10px #000;
+        color: #333; font-size: .85em; margin: 20; padding: 20;
+        font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
+    }
+    h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.75em; }
+    h3 { font-size: 1.2em; }
+    table { margin-top: 0.75em; }
+    th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
+    td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+</style>
+
 </head>
 
 <body>
@@ -51,7 +66,7 @@
 			}
 			echo "</table>";
 		} else {
-			echo "<h3>No one is currently registered with that email address.</h3>";
+			echo "<h3>No one is currently registered with that email address !</h3>";
    		 }
 	}
 ?>
